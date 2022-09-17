41fca7441e7ea3875818d40f445f388ab8eba7c9
EdwinIvanS <sedwin118@hotmail.com>
2022-09-17 14:27:11 -0500

    actualizando el proyecto

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -30,8 +30,8 @@ function Header(){
                     </div>                     
                 </div>
             </header>
-
             
+                        
                 
         </React.Fragment>       
     )
